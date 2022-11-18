706538693a55867ef1a3c239bdcae860b3e9932a
Felipe Valdez <47610639+f3l1p3v4@users.noreply.github.com>
2022-11-18 15:30:29 -0400

    Update index.js

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -118,7 +118,7 @@ class Footer extends Component {
         </div>
         <hr />
         <div className="footer-info">
-          <p>© 2021 reitec serviços de Engenharia - Todos direitos reservados.</p>
+          <p>© 2021 Reitec Serviços de Engenharia - Todos direitos reservados.</p>
           <p>
             Desenvolvido por <a href="https://felipevaldez.netlify.app">FV</a>
           </p>
